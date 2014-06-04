c0db67862680733e71bb4a159265bc304b7abbd4
rpalado <ryanpalado11@gmail.com>
2014-06-04 08:49:00 +0800

    Initial Commit

---
+++ Git/TarlacMobileApp/src/com/visittarlac/tarlacmobileapp/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.visittarlac.tarlacmobileapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends Activity {
+
+	private static String TAG = SplashActivity.class.getName();
+	private static long SLEEP_TIME = 5;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+		
+		
+		setContentView(R.layout.activity_splash);
+		
+		IntentLauncher launcher =new IntentLauncher();
+		launcher.start();
+		
+	}
+	
+	private class IntentLauncher extends Thread {
+		
+		public void run(){
+			try {
+				Thread.sleep(SLEEP_TIME * 1000);
+			}catch (Exception e) {
+				Log.e(TAG, e.getMessage());
+			}
+			
+		Intent intent= new Intent(SplashActivity.this,TravelActivity.class);
+		SplashActivity.this.startActivity(intent);
+		SplashActivity.this.finish();
+		
+		}
+	}
+}
