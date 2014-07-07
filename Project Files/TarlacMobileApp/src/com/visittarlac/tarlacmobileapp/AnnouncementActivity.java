57e214fbed81919645b8e498f23888cde3059bf2
ryanpalado <jakepalado@yahoo.com>
2014-07-07 15:18:49 +0800

    added announcement page

---
+++ Project Files/TarlacMobileApp/src/com/visittarlac/tarlacmobileapp/AnnouncementActivity.java
@@ -0,0 +1,15 @@
+package com.visittarlac.tarlacmobileapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AnnouncementActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_announcement);
+	}
+}
